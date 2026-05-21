652a80603fa1e9f95b45ccca70555dfd747c17a3
yo-huaca95 <yo.huaca@udla.edu.co>
2026-05-21 14:07:38 -0500

    Se agregaron enviroments y su path alias

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,7 +1,10 @@
 import {Component } from '@angular/core';
+import { environment } from '@enviroments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
   templateUrl: './side-menu-header.html'
 })
-export class SideMenuHeader {}
+export class SideMenuHeader {
+  envs=environment;
+}
