a055dde0b55f6b11e107b0c53be245ae171811fb
yo-huaca95 <yo.huaca@udla.edu.co>
2026-07-02 16:14:34 -0500

    Se agregan comentarios para Path alias

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,5 +1,5 @@
 import {Component } from '@angular/core';
-import { environment } from '@enviroments/environment';
+import { environment } from '@enviroments/environment';// Paht Alias
 
 @Component({
   selector: 'gifs-side-menu-header',
